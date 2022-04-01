73a9f64cb9501a9bf2f869f91277ba94cd45b728
michael shintani <mikeshintani@yahoo.com>
2022-04-01 10:02:38 -0700

    added product_image column to table + verified through postman that column gets created

--- products/models.py
+++ products/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     inventory_quantity = models.IntegerField()
+    product_image = models.CharField(max_length=255, default = "https://embed.widencdn.net/img/beef/melpznnl7q/800x600px/Top%20Sirloin%20Steak.psd?keep=c&u=7fueml")
